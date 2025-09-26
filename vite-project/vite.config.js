756b0969aeab8dc69f17711b0068e3c3c48b6bc6
Rusty Fausak <rustyfausak@gmail.com>
2025-09-26 14:16:33 -0500

    multi-page app config

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,8 +1,24 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
   plugins: [react()],
   base: '/learning/',
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        part1: resolve(__dirname, 'part1.html'),
+        part2: resolve(__dirname, 'part2.html'),
+        part3: resolve(__dirname, 'part3.html'),
+        part4: resolve(__dirname, 'part4.html'),
+        part5: resolve(__dirname, 'part5-tenzies.html'),
+        part6: resolve(__dirname, 'part6-pipes.html'),
+      },
+    },
+  },
+});
